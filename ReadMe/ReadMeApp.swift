6d6e46297f71d8154cad5cda3d3f14a7f735dd1d
mikeorr85 <miorr@paypal.com>
2022-04-12 13:46:17 -0600

    Initial Commit

---
+++ ReadMe/ReadMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadMeApp.swift
+//  ReadMe
+//
+//  Created by Orr, Mike on 4/12/22.
+//
+
+import SwiftUI
+
+@main
+struct ReadMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
